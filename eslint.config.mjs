6b304d5374011c266a8cdc81e7e7f38e424a3187
jm4293 <dlwoals4293@gmail.com>
2025-11-13 01:12:45 +0900

    feat: husky 및 lint-staged 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,16 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "no-console": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
